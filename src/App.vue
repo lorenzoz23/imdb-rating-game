d7741694fe28c66fb71f7dfc9a00d93d7b4d2a72
Lorenzo Zenitsky <lorenzoz@iastate.edu>
2020-08-20 14:35:18 -0500

    added more ui components

--- src/App.vue
+++ src/App.vue
@@ -9,15 +9,20 @@
       <!-- style="background: linear-gradient(270deg, rgba(53,86,255,1) 0%, rgba(129,203,255,1) 100%);" -->
       <v-list>
         <v-list-item class="px-2">
-          <v-list-item-avatar>
-            <v-img src="https://randomuser.me/api/portraits/women/85.jpg"></v-img>
+          <v-list-item-avatar color="blue">
+            <v-icon dark v-if="!loggedIn">mdi-account-circle</v-icon>
+            <v-img
+              src="https://cdn.vuetifyjs.com/images/john.jpg"
+              v-if="loggedIn"
+            ></v-img>
           </v-list-item-avatar>
-        </v-list-item>
-
-        <v-list-item link>
           <v-list-item-content>
-            <v-list-item-title class="title">Sandra Adams</v-list-item-title>
-            <v-list-item-subtitle>sandra_a88@gmail.com</v-list-item-subtitle>
+            <v-list-item-title class="title">{{
+              !loggedIn ? 'User' : 'Sandra Adams'
+            }}</v-list-item-title>
+            <!-- <v-list-item-subtitle>{{
+              !loggedIn ? 'log in' : ''
+            }}</v-list-item-subtitle> -->
           </v-list-item-content>
         </v-list-item>
       </v-list>
@@ -49,27 +54,47 @@
     <v-main>
       <v-card
         class="mx-auto"
-        max-width="50%"
+        max-width="60%"
         elevation="24"
         shaped
-        style="border: 5px solid lightblue; background: radial-gradient(circle, rgba(119,63,63,1) 0%, rgba(0,0,0,1) 100%);"
+        style="border: 5px solid lightblue; background: radial-gradient(circle, rgba(119,63,63,1) 0%, rgba(0,0,0,1) 100%); padding: 0px 0px 10px 0px"
       >
-        <v-tooltip top open-delay="500" close-delay="500">
-          <template v-slot:activator="{ on, attrs }">
-            <v-img
-              style="margin-top: 10px;"
-              contain
-              v-bind="attrs"
-              v-on="on"
-              height="500px"
-              width="100%"
-              src="https://m.media-amazon.com/images/M/MV5BMTUyNDIyMTA4NV5BMl5BanBnXkFtZTgwODM2MDMxNjE@._V1_SX300.jpg"
-            />
-          </template>
-          <span>The Standford Prison Experiment (2015)</span>
-        </v-tooltip>
-        <v-card-title class="justify-center" style="font-size: 32px;">
-          The Stanford Prison Experiment
+        <v-tabs
+          color="#58EE9C"
+          fixed-tabs
+          background-color="transparent"
+          dark
+          style="padding-bottom: 10px"
+        >
+          <v-tab>
+            Film
+          </v-tab>
+          <v-tab>
+            Actors
+          </v-tab>
+          <v-tab>
+            Director(s)
+          </v-tab>
+          <v-tab>
+            Writer(s)
+          </v-tab>
+        </v-tabs>
+        <!-- take away style and height to fit-->
+        <v-img
+          contain
+          height="400px"
+          src="https://m.media-amazon.com/images/M/MV5BMTUyNDIyMTA4NV5BMl5BanBnXkFtZTgwODM2MDMxNjE@._V1_SX300.jpg"
+        >
+          <template v-slot:placeholder>
+            <v-row class="fill-height ma-0" align="center" justify="center">
+              <v-progress-circular
+                indeterminate
+                color="grey lighten-5"
+              ></v-progress-circular>
+            </v-row> </template
+        ></v-img>
+        <v-card-title class="justify-center">
+          The Stanford Prison Experiment (2015)
           <!-- <v-tooltip right>
             <template v-slot:activator="{ on, attrs }">
               <v-btn
@@ -87,30 +112,39 @@
             <span>Click to visit the film's IMDb page</span>
           </v-tooltip>-->
         </v-card-title>
-        <v-card-subtitle class="pb-0" style="padding: 10px">Plot</v-card-subtitle>
+        <v-card-subtitle class="pb-0">Plot</v-card-subtitle>
         <v-card-text class="text--primary">
-          <div>In 1971, twenty-four male students are selected to take on randomly assigned roles of prisoners and guards in a mock prison situated in the basement of the Stanford psychology building.</div>
+          <div>
+            In 1971, twenty-four male students are selected to take on randomly
+            assigned roles of prisoners and guards in a mock prison situated in
+            the basement of the Stanford psychology building.
+          </div>
         </v-card-text>
-        <v-card-actions style="justify-content: center; flex-direction: column;">
-          <v-btn
-            x-large
-            rounded
-            v-if="showGuessRating"
-            color="#58EE9C"
-            dark
-            style="padding: 30px"
-            v-on:click="showGuessRating=!showGuessRating"
-          >Guess Rating</v-btn>
+        <v-card-actions
+          style="justify-content: center; flex-direction: column;"
+        >
           <v-slider
-            v-if="!showGuessRating"
-            v-model="slider"
             thumb-label="always"
+            v-model="rating"
             max="10"
             min="0"
             step=".1"
             color="#4AD589"
-            style="padding: 30px 30px 0px 30px; width: 100%"
+            append-icon="mdi-star"
+            prepend-icon="mdi-star-off"
+            @click:append="rating = rating + 0.1"
+            @click:prepend="rating = rating - 0.1"
+            style="width: 75%; margin-top: 20px"
           ></v-slider>
+          <v-btn
+            x-large
+            rounded
+            color="#58EE9C"
+            dark
+            style="padding: 0px 20px 0px 20px;"
+            v-on:click="showGuessRating = !showGuessRating"
+            >Guess IMDb Rating</v-btn
+          >
         </v-card-actions>
         <v-card-actions style="justify-content: center">
           <v-tooltip bottom>
@@ -120,8 +154,7 @@
                 v-on="on"
                 text
                 color="#4AD589"
-                @click="showGuessRating=true"
-                style="margin: 10px"
+                @click="showGuessRating = true"
               >
                 Skip
                 <v-icon style="padding-left: 10px">mdi-skip-next-circle</v-icon>
@@ -136,17 +169,18 @@
 </template>
 
 <script lang="ts">
-import Vue from "vue";
+import Vue from 'vue';
 
 export default Vue.extend({
-  name: "App",
+  name: 'App',
 
   components: {
     //
   },
 
   data: () => ({
-    showGuessRating: true
+    loggedIn: false,
+    rating: 5
   })
 });
 </script>
